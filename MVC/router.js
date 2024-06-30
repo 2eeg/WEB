f11dc8900cdcb2788715bdbacdc54eaf22715958
Jehyeok <fred0110@naver.com>
2024-07-01 00:17:25 +0900

    mod js

---
+++ MVC/router.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/list', (req, res)=>{
+
+})
+
+router.get('/write',(req,res)=>{
+
+})
+
+router.get('/view',(req,res)=>{
+    
+})
+
+router.get('/update',(req,res)=>{
+    
+})
+
+router.post('/update',(req,res)=>{
+    
+})
+
+router.get('/delete',(req,res)=>{
+    
+})
+
+module.exports = router;
